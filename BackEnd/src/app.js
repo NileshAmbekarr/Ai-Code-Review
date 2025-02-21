0bbbad8fb7887715b492bd3ffb8a9f2d67ecf23f
Nilesh Raju Ambekar <nileshambekar9281@gmail.com>
2025-02-21 12:19:11 +0530

    minor changes

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const aiRoutes = require('./routes/ai.routes')
 const app = express();
+const cors = require('cors')
 
 
 app.get('/', (req, res) =>{
     res.send('Hello, World!');
 })
+app.use(cors(
+    
+))
 app.use(express.json());
 app.use('/ai', aiRoutes)
 

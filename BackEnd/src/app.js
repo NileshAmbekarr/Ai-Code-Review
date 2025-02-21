aadb1e8fab782774254173967d7e8a2c93784d42
Nilesh Raju Ambekar <nileshambekar9281@gmail.com>
2025-02-21 12:38:15 +0530

    .

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -8,7 +8,11 @@ app.get('/', (req, res) =>{
     res.send('Hello, World!');
 })
 app.use(cors(
-    
+    {
+        origin: 'https://ai-code-review-sfe2.vercel.app/',
+        methods: ['GET', 'POST'],
+        credentials:true
+    }
 ))
 app.use(express.json());
 app.use('/ai', aiRoutes)
